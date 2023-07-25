22a6a82ce9a385bdb3f9ea7ae88117b55940ed7f
Bajpai <himanshu.bajpai@accenture.com>
2023-07-25 16:52:40 +0530

    new vpc

--- provider.tf
+++ provider.tf
@@ -1,12 +1,21 @@
-terraform{
-    backend "s3" {
-        bucket = "aws-cicd-tfstate"
-        encrypt = true
-        key = "terraform.tfstate"
-        region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
+  }
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "aws-cicd-tfstate"
+    key            = "terraform_devops/terraform.tfstate"
+    region         = "us-east-1"
+  }
 }
 
+
+
+# Configure the AWS Provider
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
